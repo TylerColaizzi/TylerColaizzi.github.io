480b5678adf0a728c75f3a9462bac9228a8f672a
Tyler Colaizzi <tycolaizzi@gmail.com>
2023-03-29 20:00:41 -0700

    Finished Assignment8

--- Assignment8/app.js
+++ Assignment8/app.js
@@ -6,10 +6,10 @@ const app = express();
 // define all endpoints here
 
 // Exercise 1
-app.get('/math/circle/:r', function (req, res) {
+app.get("/math/circle/:r", function (req, res) {
     let radius = req.params.r;
     let area = (Math.PI * (radius * radius)).toFixed(2);
-    let circumferece = (Math.PI * 2 * radius).toFixed(2);
+    let circumference = (Math.PI * 2 * radius).toFixed(2);
 
     let jInfo = { "area": area, "circumference": circumference}
 
@@ -19,14 +19,24 @@ app.get('/math/circle/:r', function (req, res) {
 });
 
 // Exercise 2
-app.get('/hello/name', (req, res) => {
-    if(req.query.first == undefined)
-    res.status(400).send('Missing Required GET parameter: first');
-    else if(req.query.first == undefined)
-    res.status(400).send('Missing Required GET parameter: last');
-    else
-        res.send('Hello ' + req.query.first + req.query.last);
-})
+app.get("/hello/name", function (req, res) {
+    if(req.query.first == undefined && req.query.last == undefined) {
+        res.status(400).json({error: "Missing Required Get parameters: first, last"});
+        return;
+    }
+    if(req.query.first == undefined) {
+        res.status(400).json({error: "Missing Required GET parameter: first"});
+        return;
+    }
+    if(req.query.first == undefined) {
+        res.status(400).json({error: "Missing Required GET parameter: last"});
+        return;
+    }
+    
 
+    res.send('Hello ' + req.query.first + ' ' + req.query.last);
+});
+
+app.use(express.static('public'));
 const PORT = process.env.PORT || 8000;
 app.listen(PORT);
